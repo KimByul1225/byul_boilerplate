5aab8732bcb18f6b59245e176cab90d939cdadc4
ByulKim <kimbyul1225@gmail.com>
2022-08-20 17:51:31 +0900

    불필요 파일 정리 및 라우터 최신화 프로젝트 마무리

--- src/router.js
+++ src/router.js
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import ErrorBoundary from "@/pages/ErrorBoundary";
-
+import Page01 from "@/pages/Page01";
+import Page02 from "@/pages/Page02";
+import Page03 from "@/pages/Page03";
+import Page04 from "@/pages/Page04";
+import Page05 from "@/pages/Page05";
 import Login from "@/pages/Login";
 import Guide from "@/pages/sample/Guide";
 import Root from "@/pages/Root";
@@ -28,6 +32,26 @@ const router = createBrowserRouter([
             {
                 path: "guide",
                 element: <Guide />,
+            },
+            {
+                path: "page01", // page01
+                element: <Page01 />,
+            },
+            {
+                path: "page02", // page02
+                element: <Page02 />,
+            },
+            {
+                path: "page03", // page03
+                element: <Page03 />,
+            },
+            {
+                path: "page04", // page04
+                element: <Page04 />,
+            },
+            {
+                path: "page05", // page05
+                element: <Page05 />,
             },
         ],
     },
